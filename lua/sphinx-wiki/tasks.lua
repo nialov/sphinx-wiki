4ad67181b6d77f491385c593a6cda523e9e62737
nialov <nikolasovaskainen@hotmail.com>
2021-08-28 12:34:42 +0300

    fix: clean up output table

--- lua/sphinx-wiki/tasks.lua
+++ lua/sphinx-wiki/tasks.lua
@@ -18,9 +18,26 @@ end
 
 function M.report_completed_tasks()
 
-    local cmd_result = vim.fn.system(full_completed_cmd)
+    local cmd_result_tbl = vim.fn.systemlist(full_completed_cmd)
+    -- Check if system command errored
     if vim.v.shell_error ~= 0 then error("Task completion report failed.") end
-    return cmd_result
+    -- Record if start of wanted lines is found
+    local start_found = false
+    -- Collect wanted lines to report_table
+    local report_table = {}
+    for _, value in pairs(cmd_result_tbl) do
+        -- First wanted line starts with Project
+        if string.find(value, "Project") then start_found = true end
+        -- If start has been found then start appending lines to report_table
+        if start_found then
+            -- Stop appending and iteration if line is empty (end of table)
+            if #value == 0 then break end
+            -- Append lines to wanted report_table
+            table.insert(report_table, value)
+        end
+    end
+    return vim.fn.join(report_table, "\n") .. "\n"
 
 end
+
 return M
