dad9fb28395a2eee8e0b460f0ad6db48b617fd02
nialov <nikolasovaskainen@hotmail.com>
2022-01-03 17:20:34 +0200

    fix(tasks): report failure or simply none complete

--- lua/sphinx-wiki/tasks.lua
+++ lua/sphinx-wiki/tasks.lua
@@ -25,24 +25,28 @@ end
 function M.report_completed_tasks_tbl()
 
     local cmd_result_tbl = vim.fn.systemlist(full_completed_cmd_tbl)
+
     -- Check if system command errored
     if vim.v.shell_error ~= 0 then
-        print("Task completion report failed or none completed within 48 hours.")
-        return ""
+        return "Report failed or none completed within 48 hours."
     end
+
     -- Record if start of wanted lines is found
     local start_found = false
+
     -- Collect wanted lines to report_table
-    local report_table = {}
-    table.insert(report_table, ".. code:: text")
-    table.insert(report_table, "")
+    local report_table = {".. code:: text", ""}
     for _, value in pairs(cmd_result_tbl) do
+
         -- First wanted line starts with Project
         if string.find(value, "Project") then start_found = true end
+
         -- If start has been found then start appending lines to report_table
         if start_found then
+
             -- Stop appending and iteration if line is empty (end of table)
             if #value == 0 then break end
+
             -- Append lines to wanted report_table
             table.insert(report_table, string.format("   %s", value))
         end
