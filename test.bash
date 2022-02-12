3aca981b50659b5a440df8033e5693bb6711dd68
nialov <nikolasovaskainen@gmail.com>
2022-02-12 14:36:28 +0200

    test: try again

--- test.bash
+++ test.bash
@@ -4,5 +4,5 @@ set -euxo pipefail
 
 export XDG_CONFIG_DIRS=""
 nix develop --unset PATH -c pre-commit run --all-files
-nix develop --unset PATH -c nvim --noplugin --headless -c 'PlenaryBustedDirectory tests' -c 'cq'
+nix develop --unset PATH -c nvim --headless -c 'PlenaryBustedDirectory tests' -c 'cq'
 
